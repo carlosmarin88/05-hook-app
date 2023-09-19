ec1c11b1b23171748d46f1f945a2576c124e4aff
Carlos Marin <carlosmarin151288@gmail.com>
2023-09-19 16:45:18 -0300

    render frase del fetch

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -3,14 +3,41 @@ import { useFetch } from '../hooks/useFetch'
 
 export const MultipleCustomHooks = () => {
 
-  const {data, isLoading, hasError} = useFetch('https://type.fit/api/quotes')
+    const { data, isLoading, hasError } = useFetch('https://type.fit/api/quotes')
 
-  console.log(data, isLoading, hasError)
+    //console.log(data, isLoading, hasError)
 
-  return (
-     <>
-        <h1>BreakingBad Quotes</h1>
-        <hr/>
-     </>
-  )
+    const {author, text} = !!data && data[0] // aca decimos si la data tiene valor entonces tomo 
+    // la posicion zero porque data hasta que tenga valor es nulo
+
+    //    if(isLoading){
+    //     return (<h1>Loading...</h1>)
+    //    } 
+
+    return (
+        <>
+            <h1>BreakingBad Quotes</h1>
+            <hr />
+            {
+                (isLoading)
+                    ? (
+                        <div className='alert alert-info text-center'>
+                            Loading...
+                        </div>
+                    ) : (
+                        <blockquote className='blockquote text-end'>
+                            <p className='mb-1'>
+                                {text}
+                            </p>
+                            <footer className='blockquote-footer mt-4'>{author}</footer>
+                        </blockquote>
+                    )
+            }
+
+            <button className='btn btn-primary'>
+                Next quote
+            </button>
+
+        </>
+    )
 }
