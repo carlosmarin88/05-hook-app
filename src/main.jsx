550aa19062089e2bf049978d6810e66d86d190b0
Carlos Marin <carlosmarin151288@gmail.com>
2023-09-22 11:08:52 -0300

    ejemplo de uso de memo

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layaout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    {/* <FormWithCustomHook /> */}
    {/* <MultipleCustomHooks /> */}
    {/* <FocusScreen/> */}
-   <Layaout />
+   {/* <Layaout /> */}
+   <Memorize/>
   </React.StrictMode>,
 )
