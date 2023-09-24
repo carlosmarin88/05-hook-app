faf04b8b8b3e4cb131a0df161445df5da252a19f
Carlos Marin <33185069+carlosmarin88@users.noreply.github.com>
2023-09-24 16:54:56 -0300

    el uso del useMemo

---
+++ src/06-memos/MemoHook.jsx
@@ -0,0 +1,35 @@
+import React, { useMemo, useState } from 'react'
+import { useCounter } from '../hooks'
+
+const heavyStuff = (iterationNumber = 100) => {
+    for (let i = 0; i < iterationNumber; i++) {
+        console.log('Ahi, vamos...')
+    }
+
+    return `${iterationNumber} iterciones realizadas`
+}
+
+export const MemoHook = () => {
+
+    const { counter, increment } = useCounter(4000)
+    const [show, setShow] = useState(true)
+
+    const memorizedValue = useMemo(()=> heavyStuff(counter), [counter])// memorizo cada vez que el valor del counter cambie, eso lo hacemos con el segundo argumento
+
+    return (
+        <>
+            <h1>Counter: <small>{counter}</small></h1>
+            <hr />
+            <h4>{memorizedValue}</h4>
+            <button
+                className='btn btn-primary'
+                onClick={() => increment()}>
+                +1
+            </button>
+            <button className='btn btn-outline-primary'
+                onClick={() => setShow(!show)}>
+                Show/Hidden {JSON.stringify(show)}
+            </button>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { HooksApp } from './hooksApp'
-import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHoolk } from './01-useState/CounterWithCustomHoolk'
-import { SimpleForm } from './02-useEffect/SimpleForm'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layaout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { CounterWithCustomHoolk } from './01-useState/CounterWithCustomHoolk'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layaout } from './05-useLayoutEffect/Layout'
+// import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -21,6 +22,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
    {/* <MultipleCustomHooks /> */}
    {/* <FocusScreen/> */}
    {/* <Layaout /> */}
-   <Memorize/>
+   <MemoHook/>
   </React.StrictMode>,
 )
