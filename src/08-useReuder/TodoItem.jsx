2d50613111f7b2d1c28e28dcafb8c7b7f922b7e4
Carlos Marin <carlosmarin151288@gmail.com>
2023-10-26 16:43:22 -0300

    Agregar test a todoItem

--- src/08-useReuder/TodoItem.jsx
+++ src/08-useReuder/TodoItem.jsx
@@ -5,11 +5,13 @@ const styleDoneTodo = (done) => done ? 'text-decoration-line-through' : ''
 export const TodoItem = ({ todo, onDeleteTodo, onToggleTodo }) => {
     return (
         <li className='list-group-item d-flex justify-content-between'>
-            <span className={`algign-self-center ${styleDoneTodo(todo.done)}`}
-                onClick={() => onToggleTodo(todo.id)}>
+            <span
+                className={`algign-self-center ${styleDoneTodo(todo.done)}`}
+                onClick={() => onToggleTodo(todo.id)}
+                aria-label='span-description'>
                 {todo.description}
             </span>
-            <button className='btn btn-danger'
+            <button aria-label='btn-delete'  className='btn btn-danger'
                 onClick={() => onDeleteTodo(todo.id)}>
                 Borrar</button>
         </li>
